cd0403ba224fe6f86ac58d871a2d6509fd7f7448
Silvio Mayolo <mercerenies@comcast.net>
2024-04-17 12:21:42 -0400

    lazy instance for infinitelist

--- src/Lambda/Util/InfiniteList.purs
+++ src/Lambda/Util/InfiniteList.purs
@@ -7,11 +7,12 @@ module Lambda.Util.InfiniteList(
                           head, tail, take, prepend, prependLazy, cycle
                          ) where
 
-import Data.Lazy (Lazy, defer, force)
+import Data.Lazy (Lazy, force)
+import Data.Lazy (defer) as Lazy
 import Data.List (List(..), (:))
 import Data.Tuple (Tuple(..))
 import Data.FunctorWithIndex (class FunctorWithIndex)
-import Control.Lazy (fix)
+import Control.Lazy (class Lazy, defer, fix)
 import Control.Comonad (class Extend, class Comonad, (=>>), extend)
 import Test.QuickCheck.Arbitrary (class Arbitrary, class Coarbitrary, arbitrary, coarbitrary)
 import Test.QuickCheck.Gen (sized)
@@ -21,15 +22,15 @@ import Prelude
 newtype InfiniteList a = InfiniteList (Lazy (Tuple a (InfiniteList a)))
 
 cons :: forall a. a -> InfiniteList a -> InfiniteList a
-cons x xs = InfiniteList (defer (\_ -> Tuple x xs))
+cons x xs = InfiniteList (Lazy.defer (\_ -> Tuple x xs))
 
 lazyCons :: forall a. a -> Lazy (InfiniteList a) -> InfiniteList a
 lazyCons x = InfiniteList <<< map (Tuple x)
 
 unfoldrForever :: forall a b. (b -> Tuple a b) -> b -> InfiniteList a
 unfoldrForever f seed = go seed
-  where go b = InfiniteList (defer \_ -> let Tuple a b' = f b in
-                                         Tuple a (go b'))
+  where go b = InfiniteList (Lazy.defer \_ -> let Tuple a b' = f b in
+                                              Tuple a (go b'))
 
 repeat :: forall a. a -> InfiniteList a
 repeat a = unfoldrForever (\_ -> Tuple a unit) unit
@@ -80,7 +81,7 @@ instance Applicative InfiniteList where
     pure = repeat
 
 instance Extend InfiniteList where
-    extend f list = lazyCons (f list) (defer \_ -> extend f (tail list))
+    extend f list = lazyCons (f list) (Lazy.defer \_ -> extend f (tail list))
 
 instance Comonad InfiniteList where
     extract = head
@@ -97,3 +98,6 @@ instance Coarbitrary a => Coarbitrary (InfiniteList a) where
     coarbitrary list gen = do
       prefix <- sized \n -> pure (take n list)
       coarbitrary prefix gen
+
+instance Lazy (InfiniteList a) where
+    defer f = InfiniteList (Lazy.defer \_ -> let InfiniteList tup = f unit in force tup)
