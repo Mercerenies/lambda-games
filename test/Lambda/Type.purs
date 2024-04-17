469c203bfe4ac152b81d5634cd5e4eab2f59545f
Silvio Mayolo <mercerenies@comcast.net>
2024-04-17 16:14:03 -0400

    basic substitute test

--- test/Lambda/Type.purs
+++ test/Lambda/Type.purs
@@ -1,10 +1,11 @@
 
 module Test.Lambda.Type(lambdaTypeSpecs) where
 
-import Lambda.Type (isClosed, makeClosed)
+import Lambda.Type (isClosed, makeClosed, freeVariables, substitute)
 
 import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec (describe, it, Spec)
+import Data.List (elem)
 import Prelude
 
 lambdaTypeSpecs :: Spec Unit
@@ -13,3 +14,7 @@ lambdaTypeSpecs = do
     describe "makeClosed" do
       it "makes a type expression closed" do
         quickCheck $ \a -> isClosed (makeClosed a)
+    describe "substitute" do
+      it "has no effect when applied to a non-free variable" do
+        quickCheck $ \ttype var substType -> var `elem` freeVariables ttype ||
+                                             ttype == substitute var substType ttype
